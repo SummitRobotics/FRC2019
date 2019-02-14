f4dfc9849a09b1bbd17721df58c30261bf6faece
Unknown <aidanb1049@gmail.com>
2019-02-14 15:48:53 -0800

    Mast Movement

--- src/main/java/frc/robot/panelClaw/clawcommands/IntakePanel.java
+++ src/main/java/frc/robot/panelClaw/clawcommands/IntakePanel.java
@@ -5,7 +5,12 @@ import frc.robot.panelclaw.*;
 import frc.robot.panelclaw.clawcommands.*;
 
 public class IntakePanel extends CommandGroup{
+    private Peg peg = Peg.getInstance();
+    private Claw claw = Claw.getIntance();
     public IntakePanel(){
+        requires(claw);
+        requires(peg);
+        
         addSequential(new ActuateClaw(Claw.ClawState.OPEN));
         addSequential(new RaiseClaw(Claw.ClawArmState.DOWN));
         addSequential(new DetectPanel());
